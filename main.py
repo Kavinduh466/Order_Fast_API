bad531a7f2dd741deed07da57fc72ea98c314452
Kavindu Herath <kavinduh466@gmail.com>
2025-10-16 01:22:30 +0530

    file changes, database connect

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from database_config import engine
+from database_config import SessionLocal
+from models.order import Base
+
 
 app = FastAPI()
 
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def greet():
