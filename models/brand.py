65838d23d578388933cabe66585458460d3df63e
Kavindu Herath <kavinduh466@gmail.com>
2025-10-29 13:14:00 +0530

    fast api without chat gpt

---
+++ models/brand.py
@@ -0,0 +1,39 @@
+
+from sqlalchemy import Column, Integer, String, Float, DateTime, Enum, ForeignKey
+from sqlalchemy.orm import relationship, declarative_base
+from datetime import datetime,timezone
+import enum
+from pydantic import BaseModel, Field, EmailStr
+from typing import List, Optional
+
+Base = declarative_base()
+
+class ProductTypeEnum(str, enum.Enum):
+    Clothes = "CLOTHES"
+    Electronic = "ELECTRONIC"
+    HandCraft = "HANDCRAFT"
+    Toys = "TOYS"
+
+class Product(Base):
+        __tablename__ = "products"
+
+        product_id  = Column(Integer, primary_key=True, index=True)
+        productType = Column(Enum(ProductTypeEnum), default=ProductTypeEnum.Electronic)
+        productName = Column(String, nullable= False)
+
+        brand_id = Column(Integer, ForeignKey("brands.brand_id"))
+
+        brand = relationship("Brand", back_populates="products")
+
+        
+
+class Brand(Base):
+    __tablename__ = "brands"
+
+    brand_id = Column(Integer, primary_key=True, index=True)
+    brand_name = Column(String, unique=True, nullable=False)
+    country = Column(String, nullable=True)
+    website = Column(String, nullable=True)
+
+    products = relationship("Product", back_populates="brand")
+
---
+++ schemas/brand.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, Field, EmailStr
+from typing import List, Optional
+from datetime import datetime
+from enum import Enum
+
+
+class ProductTypeEnum(str, Enum):
+    Clothes = "CLOTHES"
+    Electronic = "ELECTRONIC"
+    HandCraft = "HANDCRAFT"
+    Toys = "TOYS"
+
+
+class ProductItemCreate(BaseModel):
+    product_id: int
+    productType: str
+    productName: str
+    
+
+class BrandCreate(BaseModel):
+    brand_id: int 
+    brand_name: str
+    country: str
+    website: str
+    products: List[ProductItemCreate]
+
+
+
